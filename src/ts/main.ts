852d8fd64e24d1b646ce84e0a9e0892086fec7b6
asaiasa <okakenaoshikudasai@gmail.com>
2020-04-18 16:01:21 +0900

    del debug function

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,13 +2,6 @@ import { app, BrowserWindow } from "electron";
 import * as path from "path";
 import DM from './dataManager';
 
-/**************************************************************************************/
-import { mydebug as mydebug } from './debug';
-const DEBUG: boolean = true;    // all store data is deleted when application is closed.
-const TEST_MINUTES: boolean = true;
-const TEST_AGENDA: boolean = true;
-/**************************************************************************************/
-
 var mainWindow: Electron.BrowserWindow | null = null;
 
 // correspond to default value deprecated 
@@ -26,14 +19,6 @@ function createWindow() {
 
     // and load the index.html of the app.
     mainWindow.loadFile(path.join(__dirname, "../src/html/index.html"));
-
-    if (DEBUG) {
-        // Open the DevTools.
-        mainWindow.webContents.openDevTools();
-        mainWindow.maximize();
-        if (TEST_MINUTES) mydebug.Store_Sample_Minutes_Data();
-        if (TEST_AGENDA) mydebug.Store_Sample_Agenda_Data();
-    }
 
     // Emitted when the window is closed.
     mainWindow.on("closed", () => {
@@ -75,10 +60,6 @@ app.on("activate", () => {
 // code. You can also put them in separate files and require them here.
 function postProcess() {
     const dm: DM = new DM();
-    if (DEBUG) {
-        dm.console_all();
-        dm.del_all();
-    }
     dm.del_New();
     dm.del_Temp();
     dm.del_free();
